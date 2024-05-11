039650257930fbb256e3462904edcbabfa4c14cc
yhosutun2491 <yhosutun2491@gmail.com>
2024-05-11 21:55:45 +0800

    <feat> 增加focus樣式和更改內容

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,10 @@
   </h1>
   </header>
   <div class="h-3/5">
-    <tree-chart class="mx-auto" :data="treeData[0]"/>
+    <tree-chart 
+    class="mx-auto" :data="tree.root" 
+    @getCurrentKey="(id)=>getCurrentKey(id)"
+    />
   </div>
   <option-modal class="mt-auto flex space-x-[10px] p-5">
     <div 
@@ -17,100 +20,68 @@
       {{ item.content}}
     </div>
   </option-modal>
+  <div>
+    目前節點資料 id: {{ activeNodeKey }}
+    <label for="">title</label>
+    <input type="text" v-model="currentNodeData.value.title">
+    <label for="">content</label>
+    <input type="text" v-model="currentNodeData.value.content">
+  </div>
 </div>
-
-
 </template>
 <script setup>
-import {ref} from 'vue'
+import {ref,computed} from 'vue'
 import TreeChart from './components/TreeChart.vue'
 import OptionModal from './components/OptionModal.vue'
-const treeData = ref([
-  {
-    id: 1, 
-    title: '觸發事件',
-    content: '點擊email',
-    children: [
-      { 
-        id: 1, 
-        title: '觸發事件',
-        content: '點擊email'
-      },
-      { 
-        id: 2, 
-        title: '觸發事件',
-        content: '點擊email',
-        children: [
-          { 
-            id: 1, 
-            title: '觸發事件',
-            content: '點擊email',
-            children: [
-            { 
-              id: 5, 
-              title: '觸發事件',
-              content: '點擊email'
-            }
+import {Tree} from './utility/Tree'
+const tree = ref(new Tree(1, {
+  title: '觸發事件',
+  content: '點擊email'
+}))
+// 目前點擊節點的key
+const activeNodeKey= ref(1)
 
-            ]
-          },
-          { 
-            id: 2, 
-            title: '觸發事件',
-            content: '點擊email',
-            children: [
-            { 
-              id: 5, 
-              title: '觸發事件',
-              content: '點擊email'
-            },
-            { 
-              id: 5, 
-              title: '觸發事件',
-              content: '點擊email'
-            }]
-          },
-        ]
-        
-      },
-      { 
-        id: 3, 
-        title: '觸發事件',
-        content: '點擊email',
-        children: [
-          { 
-            id: 1, 
-            title: '觸發事件',
-            content: '點擊email',
-            children: [
-            { 
-              id: 5, 
-              title: '觸發事件',
-              content: '點擊email'
-            }
+tree.value.insert(1, 11, {
+  title: '觸發事件',
+  content: '點擊email'
+});
+tree.value.insert(11, 111, {
+  title: '觸發事件',
+  content: '點擊廣告'
+});
+tree.value.insert(1, 12, {
+  title: '觸發事件',
+  content: '點擊email'
+});
+tree.value.insert(12, 121, {
+  title: '觸發事件',
+  content: '點擊email'
+});
+tree.value.insert(12, 122, {
+  title: '觸發事件123',
+  content: '點擊email123'
+});
+tree.value.insert(12, 123, {
+  title: '觸發事件123',
+  content: '點擊email123'
+});
+function getCurrentKey(key) {
+  activeNodeKey.value = Number(key)
+  // 將該節點focus 增加active = true
+  tree.value.activeNodeFocus(Number(key))
+}
+function addTreeNode() {
+  tree.value.insert(11,111,{
+    title: '觸發事件New',
+    content: '點擊SMS'
+  })
+}
+const currentNodeData = computed(()=>{
+  return tree.value.find(activeNodeKey.value)
+})
 
-            ]
-          },
-          { 
-            id: 2, 
-            title: '觸發事件',
-            content: '點擊email'
-          },
-        ]
-      }
-  ]
-  },
-  {
-    id: 2, 
-    title: '觸發事件',
-    content: '點擊email'
-  },
-  {
-    id: 3, 
-    title: '觸發事件',
-    content: '點擊email'
-  }
-])
+
+
 const buttonOption = ref([
   {
     id: 1,
@@ -155,4 +126,5 @@ header {
     flex-wrap: wrap;
   }
 }
-</style>
+</style>import { data } from 'autoprefixer';
+
--- src/components/TreeChart.vue
+++ src/components/TreeChart.vue
@@ -5,28 +5,32 @@
     <td colspan=8 class="parent relative flex justify-center">
       <div 
       class="card w-[100px] h-[100px] bg-blue bg-slate-50 hover:cursor-pointer"
-      @click="($event)=>blockOnfocus($event)"
-      @mouseleave.self="($event)=>removeFocus($event)"
+      @click="(event)=>getCurrentKey(event)"
+      :data-id="props.data.key"
+      :class="[props.data.value.isActive?'isActive':'']"
       >
-        <div class="header">{{ props.data.title }}</div>
-        <div class="body">{{ props.data.content}}</div>
+        <div class="header">{{ props.data.value.title }}</div>
+        <div class="body">{{ props.data.value.content}}</div>
         <div class="footer">按鈕</div>
       </div>
     </td>
   </tr>
   <!--子元素-->
   <tr class="flex justify-around mt-[45px] w-[100%]" v-if="props.data.children">
-    <td class="child relative w-[100%] flex justify-center"  v-for="item in props.data.children" :key="item.id">
+    <td class="card relative w-[100%] flex justify-center"  v-for="item in props.data.children" :key="item.key"
+    :class="[props.data.children.length===1?'one-child':'child']"
+    >
       <TreeChart
       :data="item"
-      v-if="item.title"
+      v-if="item.value"
+      @getCurrentKey="(id)=>getCurrentKeyEmit(id)"
       />
     </td>
   </tr>
  </table>
 </template>
 <script setup>
-import {defineProps } from 'vue'
+import {defineProps,defineEmits } from 'vue'
 import TreeChart from './TreeChart.vue'
 const props = defineProps({
   data: {
@@ -34,17 +38,30 @@ const props = defineProps({
     default: ()=>{}
   }
 })
-function blockOnfocus(e) {
+const emits = defineEmits(['getCurrentKey'])
+function getCurrentKeyEmit(id) {
+  emits('getCurrentKey',id)
+
+}
+
+
+function getCurrentKey(e) {
+  let id = ''
   if (Array.from(e.target.classList).includes('card')) {
     e.target.classList.add('focus-animate')
+    id = e.target.dataset.id
+    console.log("e",e.target.dataset.id)
+
   } else {
     e.target.parentNode.classList.add('focus-animate')
+    id = e.target.parentNode.dataset.id
+    console.log("e",e.target.parentNode.dataset.id)
   }
+  emits('getCurrentKey',id)
+
 }
-function removeFocus(e) {
-  e.target.classList.remove('focus-animate')
-}
-console.log("props",props)
+
+
 </script>
 
 <style scoped>
@@ -79,6 +96,17 @@ console.log("props",props)
   top: -15px;
   border-top: 2px dashed rgb(159,186,202);
 }
+/*單一child*/
+.one-child::before {
+  content: '';
+  position: absolute;
+  left: 50%;
+  bottom: 100%;
+  height: 15px;
+  border-left: 2px dashed rgb(159,186,202);
+  transform: translate3d(-1px,0,0);
+}
+
 /* 前後第一個和最後一個元素橫線修飾蓋掉 */
 .child:first-child::after {
   left: 50%;
@@ -96,7 +124,7 @@ console.log("props",props)
   border-radius: 0 6px 0 0;
   transform: translate3d(1px,0,0);
 }
-.focus-animate {
+.isActive {
   border: 2px solid gold;
 
 }
